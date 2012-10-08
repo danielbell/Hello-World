1b762899651aad379235c05d630211ba124446c9
DBELL <dbell@innoveer.com>
2012-10-08 14:14:18 +0100

    sctipt for chat window

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 <li>body text.......</li>
 <li>Line2.......</li>
 </ol>
+
+<script type='text/javascript' src='https://c.la1w1.salesforceliveagent.com/content/g/deployment.js'></script>
+<script type='text/javascript'>
+liveagent.init('https://d.la1w1.salesforceliveagent.com/chat', '572G000000000LT', '00DG0000000BtbR');
+</script>
+
 </body>
 </html>
