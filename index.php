677505bbe2a053fe155be3e901eeddb806a7984d
DBELL <dbell@innoveer.com>
2012-10-08 14:24:08 +0100

    chat button script

--- index.php
+++ index.php
@@ -9,6 +9,14 @@
 <li>Line2.......</li>
 </ol>
 
+
+<a id="liveagent_button_online_573G000000000L9" href="javascript://Chat" style="display: none;" onclick="liveagent.startChat('573G000000000L9')"><!-- Online Chat Content --></a><div id="liveagent_button_offline_573G000000000L9" style="display: none;"><!-- Offline Chat Content --></div><script type="text/javascript">
+if (!window._laq) { window._laq = []; }
+window._laq.push(function(){liveagent.showWhenOnline('573G000000000L9', document.getElementById('liveagent_button_online_573G000000000L9'));
+liveagent.showWhenOffline('573G000000000L9', document.getElementById('liveagent_button_offline_573G000000000L9'));
+});</script>
+
+
 <script type='text/javascript' src='https://c.la1w1.salesforceliveagent.com/content/g/deployment.js'></script>
 <script type='text/javascript'>
 liveagent.init('https://d.la1w1.salesforceliveagent.com/chat', '572G000000000LT', '00DG0000000BtbR');
